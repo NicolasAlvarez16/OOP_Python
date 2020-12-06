e3b2a3cb9336c207b89ed92c2325872ce1155abd
NicolasAlvarez16 <59146295+NicolasAlvarez16@users.noreply.github.com>
2020-12-06 18:43:17 +0000

    Commit 1

---
+++ Exercise_1.py
@@ -0,0 +1,22 @@
+# Introduction to Classes
+
+class Employee():
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay  = pay
+        self.email = first + '.' + last + "@company.com" 
+
+    def full_name(self):
+        return "{} {}".format(self.first, self.last)
+
+
+emp_1 = Employee("Nicolas", "Alvarez", 50000)
+emp_2 = Employee("Test", "User", 60000)
+
+# print(emp_1)
+# print(emp_2)
+
+print(emp_1.email)
+print(emp_2.email)
+print(emp_1.full_name())
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Object Orientated Programming
 
+## Classes
+They allow us to logically group our data (attributes) and functions (methods) in a way that's is easy to reause and also easy to build upon if needed.
+A class is basically a blueprint for creating intances. Each unique object that is created using a certain class will be an instance of tha class.
+
+## Instances
+Instance variables contain data that is unique to each instance.
